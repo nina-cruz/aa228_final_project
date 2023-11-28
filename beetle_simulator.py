12974424c606436cbcfdfd1b7d3eb2878817feac
Sixten Norelius <sixtennorelius@gmail.com>
2023-11-28 14:43:19 -0800

    Added plotting and randomly generated landscapes functionality

--- beetle_simulator.py
+++ beetle_simulator.py
@@ -1,21 +1,21 @@
 import numpy as np
 import random
+import utils
 
 class Simulator:
-    def __init__(self, n, empty_locations, beetle_source):
+    def __init__(self, trees, beetles):
         # n: size of grid (nxn)
         # empty_locations: tuples of indices where no trees
         # beetle_source: tuple of beetle source square
-        self.trees = np.ones((n,n))*5000
-        for loc in empty_locations:
-            self.trees[loc] = 0
-        self.source = beetle_source
-        self.beetles = np.zeros((n,n))
-        self.beetles[beetle_source] = 200
-        self.n = n
+        
+        self.trees = trees
+        self.beetles = beetles
+        self.n = trees.shape[0]
+
+        
 
         # parameters
-        self.beetle_repl_rate = 1.1
+        self.beetle_repl_rate = 1.3#1.1
         self.beetle_decay_rate = 0.7
 
         # parameters for Dirichlet beetle movement prob from empty square
@@ -52,27 +52,26 @@ class Simulator:
         # create temporary grid to store new beetle counts (need to preserve intial beetle counts to multiply)
         temp_beetles = np.zeros_like(self.beetles)
         directions = np.array([[-1,0],[1,0],[0,-1],[0,1]]) # left, right, above, below
-        for i in range(0, self.n):
-            for j in range(0, self.n):
-                cur = np.array([i,j])
-                
-                neighbours = [cur] # Create list of valid neighbours (including the current square)
-                for direction in directions: 
-                    potentialNeighbour = cur + direction
-                    if self.withinBounds(potentialNeighbour):
-                        neighbours.append(potentialNeighbour)        
-                        
-                originStatus = int(self.trees[*cur] > 0) # 0 if empty, 1 if tree exists
-                params = [self.stay_status[originStatus]] # Staying put comes first in params list
-                for neighbour in neighbours:
-                    neighbourStatus = int(self.trees[*neighbour] > 0)
-                    params.append(self.status_to_status[originStatus, neighbourStatus]) # Set dirichlet parameters for each neighbour
+        
+        nonzero = np.argwhere(self.beetles)
+        for cur in nonzero:
+            neighbours = [cur] # Create list of valid neighbours (including the current square)
+            for direction in directions: 
+                potentialNeighbour = cur + direction
+                if self.withinBounds(potentialNeighbour):
+                    neighbours.append(potentialNeighbour)        
+                    
+            originStatus = int(self.trees[*cur] > 0) # 0 if empty, 1 if tree exists
+            params = [self.stay_status[originStatus]] # Staying put comes first in params list
+            for neighbour in neighbours:
+                neighbourStatus = int(self.trees[*neighbour] > 0)
+                params.append(self.status_to_status[originStatus, neighbourStatus]) # Set dirichlet parameters for each neighbour
 
-                square_probs = np.random.dirichlet(params)
-                squareAddBeetles = np.floor(square_probs*self.beetles[*cur]) # Beetles that move to each neighbour
+            square_probs = np.random.dirichlet(params)
+            squareAddBeetles = np.floor(square_probs*self.beetles[*cur]) # Beetles that move to each neighbour
 
-                for square, movedBeetles in zip(neighbours, squareAddBeetles):
-                    temp_beetles[*square] += movedBeetles
+            for square, movedBeetles in zip(neighbours, squareAddBeetles):
+                temp_beetles[*square] += movedBeetles
 
         self.beetles = temp_beetles
 
@@ -88,12 +87,13 @@ class Simulator:
 
 
 def main():
-    sim = Simulator(10,[(0,5),(0,2),(2,6),(2,7),(3,2),(4,9),(3,9),(8,7)],(0,0))
+    forest, beetles = utils.generateRandomForest(10, 2, 100, 5, 5000)
+    sim = Simulator(forest, beetles)
 
-    for i in range(10):
+    for i in range(1000):
         sim.simulate_timestep()
-    print(sim.beetles)
-    print(sim.trees)
+    
+    utils.plotTrees(sim.trees)
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+
+
+def generateRandomForest(nLength, nSources, nBeetles, nEmpty, maxHealth):
+    '''Method for generating a forest on which to run the simulator'''
+    rng = np.random.default_rng()
+    trees = np.ones((nLength, nLength))*maxHealth
+    beetles = np.zeros((nLength, nLength))
+
+    # The lines below select the prescribed number of empty and source squares and set them accordingly
+    emptySquares = np.unravel_index(rng.choice(trees.size, replace=False, size=nEmpty), trees.shape)
+    trees[emptySquares] = 0
+
+    sourceSquares = np.unravel_index(rng.choice(beetles.size, replace=False, size=nSources), beetles.shape)
+    beetles[sourceSquares] = nBeetles
+    
+    return trees, beetles
+
+
+def plotTrees(trees):
+    #cmap = mpl.colors.ListedColormap(["green", "blue"])
+    #bounds = 
+
+
+    trees = ((trees / 5000) * 100).astype(int)
+    plt.figure(figsize=trees.shape)
+    plt.imshow(trees, cmap=mpl.colormaps['viridis'])
+    plt.show()
+
+def plotBeetles(beetles):
+    pass
+
+
+def main():
+    forest, beetles = generateRandomForest(10, 2, 100, 5, 5000)
+    plotTrees(forest)
+
+
+if __name__ == "__main__":
+    main()
+
